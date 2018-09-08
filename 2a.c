40c7e4d77463f5b14547f8566065ac562759abb6
Terts Diepraam <terts.diepraam@gmail.com>
2018-09-08 14:21:32 +0200

    Changed second max3 implementation

--- 2a.c
+++ 2a.c
@@ -21,11 +21,10 @@ int max3(int a, int b, int c) {
     return max2(a, max2(b, c));
 }
 
-// Contributor: Floris van den Ende
 int max3(int a, int b, int c) {
-    if (a>=b && a>c) {
+    if (a>=b && a>=c) {
         return a;
-    } else if (b>a && b>c) {
+    } else if (b>c) {
         return b;
     } else {
         return c;
